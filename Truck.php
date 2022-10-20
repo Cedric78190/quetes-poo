8e854d988f7514975350fde5e5272b98b6deff99
Cedric Nkene <cedric.nkn@gmail.com>
2022-10-20 19:32:48 +0200

    quete poo

---
+++ Truck.php
@@ -0,0 +1,50 @@
+<?php
+
+class Truck extends Vehicule
+{
+    public const ALLOWED_ENERGIES = [
+        'essence',
+        'electric'
+    ];
+
+    private int $capacity;
+    private int $filling = 0;
+    private string $energy;
+
+    public function __construct(string $color, int $nbSeats, string $energy, int $capacity){
+        parent::__construct($color, $nbSeats);
+        $this->setCapacity($capacity);
+        $this->setEnergy($energy);
+    }
+
+    public function setEnergy(string $energy): Truck{
+        if(in_array($energy, self::ALLOWED_ENERGIES)){
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+   
+
+    public function setFilling(int $filling): void {
+        $this->filling = $filling;
+    }
+
+    public function infilling(): string 
+    {
+    if($this->filling < $this->capacity){
+        return 'in filing';
+    }else{
+        return 'full';
+    }
+    }
+    public function getCapacity(){
+        return $this->$capacity;
+    }
+
+    public function setCapacity(int $capacity): void {
+           if($capacity > 0){
+             $this->capacity = $capacity;
+           }
+    }
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'Vehicule.php';
+require 'Bicycle.php';
+require 'Car.php';
+require 'Truck.php';
+
+
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+var_dump($car);
+
+var_dump(Car::ALLOWED_ENERGIES);
+
+$truck = new Truck('yellow', 6, 'essence', 20);
+$truck->setCurrentSpeed(20);
+$truck->setFilling(30);
+var_dump($truck);
+echo $truck->brake(); ?> <br>
+<?php
+echo $truck->inFilling(); 
